4ed2c4341441d2cac714f2852e2a6a5e7e3a3ecf
DamienEvrard <damien.evrard@etud.univ-jfc.fr>
2021-04-01 17:17:12 +0200

    dao terminés pas de requete dans piece mais dans capteur

--- src/main/java/daos/CapteurRepository.java
+++ src/main/java/daos/CapteurRepository.java
@@ -1,5 +1,6 @@
 
 package daos;
+import entities.Capteur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
@@ -18,4 +19,21 @@ public class CapteurRepository extends JpaRepository<Capteur, Integer>{
      */
     @Query("SELECT M.value from Mesure M, Capteur C where C.id = M.capteur.id and M.date = :date")
     Float[] getMesure(Date date);
+    
+    /**
+     * 
+     * @param id id de la piece
+     * @return le tableau des valeurs relevées de la piece 
+     */
+    @Query("SELECT M.value from Mesure M, Capteur C, Piece P where C.id = M.capteur.id and P:id = : id")
+    Float[] getPenibility(int id);
+    
+    /**
+     * 
+     * @param id id de la piece
+     * @param date id de la piece
+     * @return le tableau des valeurs relevées de la piece 
+     */
+    @Query("SELECT M.value from Mesure M, Capteur C, Piece P where C.id = M.capteur.id and P:id = :id and M.date = :date")
+    Float[] getPenibility(int id, Date date);
 }
--- src/main/java/daos/PieceRepository.java
+++ src/main/java/daos/PieceRepository.java
@@ -5,5 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public class PieceRepository extends JpaRepository<Piece, Integer>{
     
-    
+    /**
+     * 
+     * @return le tableau des valeurs relevées de la piece 
+     */
+    @Query("SELECT M.value from Mesure M, Capteur C where C.id = M.capteur.id")
+    Float[] getPenibility();
 }
