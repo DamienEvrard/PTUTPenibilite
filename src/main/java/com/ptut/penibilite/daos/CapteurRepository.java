6065c77eac24ad44522994673c78e7c1dd0ed099
DamienEvrard <damien.evrard@etud.univ-jfc.fr>
2021-04-14 18:12:27 +0200

    bdd generee, dao pas de fonction, pas de jeu de test

--- src/main/java/com/ptut/penibilite/daos/CapteurRepository.java
+++ src/main/java/com/ptut/penibilite/daos/CapteurRepository.java
@@ -10,18 +10,19 @@ import org.springframework.data.jpa.repository.Query;
 public interface CapteurRepository extends JpaRepository<Capteur, Integer>{
     
     /**
-     * 
+     * @param id l'id du capteur
      * @return le tableau des mesures relevées
      */
-    @Query("SELECT M.id, M.date, M.value from Mesure M, Capteur C where C.id = M.capteur.id")
-    Mesure[] getMesure();
+//    @Query("SELECT M.id, M.date, M.value from Mesure M, Capteur C where M.capteur.id = :id")
+//    Mesure[] getMesure(int id);
     
     /**
+     * @param id l'id du capteur
      * @param date la date de debut de relevé
      * @return le tableau des mesures relevées à partir de la date 
      */
-    @Query("SELECT M.id, M.date, M.value from Mesure M, Capteur C where C.id = M.capteur.id and M.date = :date")
-    Mesure[] getMesure(Date date);
+//    @Query("SELECT M.id, M.date, M.value from Mesure M, Capteur C where M.capteur.id = :id and M.date = :date")
+//    Mesure[] getMesure(int id, Date date);
     
     
 }
--- src/main/java/com/ptut/penibilite/daos/PieceRepository.java
+++ src/main/java/com/ptut/penibilite/daos/PieceRepository.java
@@ -13,16 +13,16 @@ public interface PieceRepository extends JpaRepository<Piece, Integer>{
      * 
      * @return le tableau des valeurs relevées de la piece 
      */
-    @Query("SELECT M.value from Mesure M, Capteur C where C.id = M.capteur.id")
-    Float[] getPenibility();
+//    @Query("SELECT M.value from Mesure M, Capteur C where C.id = M.capteur.id")
+//    Float[] getPenibility();
     
     /**
      * 
      * @param id id de la piece
      * @return le tableau des mesures relevées de la piece 
      */
-    @Query("SELECT M.id, M.date, M.value from Mesure M, Capteur C, Piece P where C.id = M.capteur.id and P:id = : id")
-    Mesure[] getPenibility(int id);
+//    @Query("SELECT M.id, M.date, M.value from Mesure M, Capteur C, Piece P where C.id = M.capteur.id and P:id = : id")
+//    Mesure[] getPenibility(int id);
     
     /**
      * 
@@ -30,6 +30,6 @@ public interface PieceRepository extends JpaRepository<Piece, Integer>{
      * @param date la date de debut de relevé 
      * @return le tableau des mesures relevées de la piece 
      */
-    @Query("SELECT M.id, M.date, M.value from Mesure M, Capteur C, Piece P where C.id = M.capteur.id and P:id = :id and M.date = :date")
-    Mesure[] getPenibility(int id, Date date);
+//    @Query("SELECT M.id, M.date, M.value from Mesure M, Capteur C, Piece P where C.id = M.capteur.id and P:id = :id and M.date = :date")
+//    Mesure[] getPenibility(int id, Date date);
 }
