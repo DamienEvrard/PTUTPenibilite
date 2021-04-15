f6d88c0f022088c0e6ad6dd582596b0cefd0a638
MarieCagnasso <marie.cagnasso@etud.univ-jfc.fr>
2021-04-16 00:20:08 +0200

    page pièce

---
+++ src/main/java/com/ptut/penibilite/controllers/PieceController.java
@@ -0,0 +1,24 @@
+package com.ptut.penibilite.controllers;
+
+import com.ptut.penibilite.daos.PieceRepository;
+import com.ptut.penibilite.entities.Piece;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path = "/piece")
+public class PieceController {
+
+    @Autowired
+    private PieceRepository pieceRepository;
+
+    @GetMapping("")
+    public String getPiece(Model model, @RequestParam("id") Piece piece){
+        model.addAttribute("pieces", pieceRepository.findAll());
+        model.addAttribute("piece", piece);
+        model.addAttribute("capteurs",piece.getCapteurs());
+        return "piece";
+    }
+}
