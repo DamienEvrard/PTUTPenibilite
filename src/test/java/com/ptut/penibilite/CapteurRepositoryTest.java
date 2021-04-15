4aa972dff8cc19baa7baf8d02436dcc39f1c0732
MarieCagnasso <marie.cagnasso@etud.univ-jfc.fr>
2021-04-15 14:47:37 +0200

    variable dao

--- src/test/java/com/ptut/penibilite/CapteurRepositoryTest.java
+++ src/test/java/com/ptut/penibilite/CapteurRepositoryTest.java
@@ -1,5 +1,17 @@
 package com.ptut.penibilite;
 
+import com.ptut.penibilite.daos.CapteurRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@Log4j2
+@DataJpaTest
 public class CapteurRepositoryTest {
-    
+
+    @Autowired
+    private CapteurRepository dao;
+
+
+
 }
