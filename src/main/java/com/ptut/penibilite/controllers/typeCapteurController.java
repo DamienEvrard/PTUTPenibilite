5c4c34c5c03c95931131c96674c8e5f69cff6a6d
MarieCagnasso <marie.cagnasso@etud.univ-jfc.fr>
2021-04-17 13:14:51 +0200

    act modifier type capteur.

--- src/main/java/com/ptut/penibilite/controllers/typeCapteurController.java
+++ src/main/java/com/ptut/penibilite/controllers/typeCapteurController.java
@@ -55,4 +55,67 @@ public class typeCapteurController {
         redirectInfo.addFlashAttribute("message", message);
         return "redirect:add";
     }
+
+    /**
+     * Affiche le formulaire de modification d'un type capteur
+     *
+     * @param typeCapteur modèle de donnée pour le formulaire
+     * @param myType le type à modifier
+     * @return vue formModifySensorType.html
+     */
+    @GetMapping(path = "modify")
+    public String addCapteur(Model model, @ModelAttribute("typeC") TypeCapteur typeCapteur,@RequestParam("id") TypeCapteur myType) {
+        model.addAttribute("pieces", pieceRepository.findAll());
+        model.addAttribute("types", typeCapteurRepository.findAll());
+        model.addAttribute("type", myType);
+        return "formModifySensorType";
+    }
+
+    /**
+     * Appelé par 'formModifySensorType.html', méthode POST
+     *
+     * @param type Un type de capteur initialisée avec les valeurs saisies dans le formulaire
+     * @param redirectInfo pour transmettre des paramètres lors de la redirection
+     * @return une redirection vers le formulaire si pas de modification
+     * @return une redirection vers le formAjoutTypeCapteur si modifier
+     */
+    @PostMapping(path = "modify/save/{id}")
+    public String addCapteur(TypeCapteur type, RedirectAttributes redirectInfo,@PathVariable int id) {
+        String message;
+        int cpt = 0;
+        TypeCapteur t = typeCapteurRepository.getOne(id);
+
+        if (!type.getLibelle().isEmpty() && type.getLibelle() != t.getLibelle()){
+            t.setLibelle(type.getLibelle());
+            cpt++;
+        }
+        if (type.getLimiteMin() != t.getLimiteMin()){
+            t.setLimiteMin(type.getLimiteMin());
+            cpt++;
+        }
+        if (type.getLimiteMax() != t.getLimiteMax()){
+            t.setLimiteMax(type.getLimiteMax());
+            cpt++;
+        }
+       /* if (!type.getUnite().isEmpty() && type.getUnite() != t.getUnite()){
+            t.setUnite(type.getUnite());
+            cpt++;
+        }*/
+
+        if (cpt>0){
+            try {
+                typeCapteurRepository.save(t);
+                message = "Les modification du type '" + t.getLibelle() + "' ont été correctement enregistrées";
+            } catch (DataIntegrityViolationException e) {
+                message = "Erreur mis-à-jour : du type '" + t.getLibelle();
+            }
+            redirectInfo.addFlashAttribute("message", message);
+            return "redirect:/type/add";
+        }else {
+            message = "Aucune modification apporté pour le type '" + t.getLibelle();
+            redirectInfo.addFlashAttribute("message", message);
+            return "redirect:/type/modify?id="+id;
+        }
+
+    }
 }
