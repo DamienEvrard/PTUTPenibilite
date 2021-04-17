c803d50b5e778474c8db2aec4e3c7b7f8ae055c9
MarieCagnasso <marie.cagnasso@etud.univ-jfc.fr>
2021-04-18 00:55:16 +0200

    redirection apres suppression.

--- src/main/resources/static/assets/js/deleteAct.js
+++ src/main/resources/static/assets/js/deleteAct.js
@@ -1,7 +1,7 @@
 let url = 'http://localhost:8080/';
 
 function deleteType(id) {
-    if (confirm('êtes-vous sûr de vouloir supprimer ce type ?' +id)){
+    if (confirm('êtes-vous sûr de vouloir supprimer ce type ?')){
 
         $.ajax({
             url: url + 'api/type/delete' ,
@@ -12,31 +12,31 @@ function deleteType(id) {
         })
         .done((data)=> {
             if(data.status === 0){
-                window.location.reload();
+                window.location.replace(url+"type/add");
             }else  {
                 document.getElementById("msg").innerHTML = "Vous ne pouvez pas supprimer ce type car des capteurs y sont associés.";
             }
         })
         .fail((data) => {
-            console.log("fail" , data);
+            console.log("fail" , data,data.responseText);
         })
 
     }
 }
 
-function deleteCapteur(id) {
-    if (confirm('êtes-vous sûr de vouloir supprimer ce type ?' +id)){
+function deleteCapteur(idCapteur,idSalle) {
+    if (confirm('êtes-vous sûr de vouloir supprimer ce type ?')){
 
         $.ajax({
             url: url + 'api/capteur/delete' ,
             data : {
-                id : id
+                id : idCapteur
             },
             method: 'DELETE'
         })
             .done((data)=> {
                 if(data.status === 0){
-                    window.location.reload();
+                    window.location.replace(url+"capteur/add?id="+idSalle);
                 }else  {
                     document.getElementById("msg").innerHTML = "Vous ne pouvez pas supprimer ce capteur car des mesures y sont associées.";
                 }
