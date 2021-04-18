fdc6be89b67c0b3899cdcf151c85d6d08d04efd5
DamienEvrard <damien.evrard@etud.univ-jfc.fr>
2021-04-18 20:42:16 +0200

    getTypeCapteur Piece entité

--- src/main/java/com/ptut/penibilite/entities/Piece.java
+++ src/main/java/com/ptut/penibilite/entities/Piece.java
@@ -1,7 +1,12 @@
 package com.ptut.penibilite.entities;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.*;
 import lombok.*;
+import net.minidev.json.JSONObject;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Getter @Setter @NoArgsConstructor @RequiredArgsConstructor @ToString
 @Entity
@@ -15,4 +20,16 @@ public class Piece {
     
     @OneToMany(mappedBy="salle")
     private List<Capteur> capteurs;
+
+    /**
+     *
+     * @return la liste de stype de capteur dans la salle
+     */
+    public List<String> getTypeCapteur(){
+        List<String> listType = new ArrayList<>();
+        for(Capteur c : this.getCapteurs()){
+            listType.add(c.getType().getLibelle());
+        }
+        return listType;
+    }
 }
