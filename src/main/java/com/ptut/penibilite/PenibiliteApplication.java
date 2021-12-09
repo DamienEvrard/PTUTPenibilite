c243c2d39edc1aefc887ee8251b8a3baf5a014ea
DamienEvrard <damien.evrard@etud.univ-jfc.fr>
2021-12-09 10:54:09 +0100

    test

--- src/main/java/com/ptut/penibilite/PenibiliteApplication.java
+++ src/main/java/com/ptut/penibilite/PenibiliteApplication.java
@@ -3,6 +3,7 @@ package com.ptut.penibilite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//application a lancer
 @SpringBootApplication
 public class PenibiliteApplication {
 
